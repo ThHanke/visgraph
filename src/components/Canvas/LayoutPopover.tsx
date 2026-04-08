8eda6adbf1e3a56d0025b354d9499b4ddfe3bf69
Thomas Hanke <thomas.hanke@iwm.fraunhofer.de>
2026-04-08 16:29:02 +0200

    fix: remove line-height: 1 from glass-btn so all buttons inherit Reactodia line-height

--- src/components/Canvas/LayoutPopover.tsx
+++ src/components/Canvas/LayoutPopover.tsx
@@ -56,7 +56,7 @@ export const LayoutPopover: React.FC<LayoutPopoverProps> = ({ onApplyLayout }) =
   return (
     <Popover>
       <PopoverTrigger asChild>
-        <button className="reactodia-btn reactodia-btn-default glass-btn" title="Layout settings" style={{ lineHeight: 1 }}>
+        <button className="reactodia-btn reactodia-btn-default glass-btn" title="Layout settings">
           <Layout style={{ width: 14, height: 14 }} />
           <span>Layout</span>
         </button>
